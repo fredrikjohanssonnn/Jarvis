c29dc4056593f280391436fdf53ded79949ebe01
fredrikjohanssonnn <fjohansson.creative@gmail.com>
2020-04-19 14:37:47 +0200

    Setup connection to bot

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const token = process.env.token;
+const dotenv = require('dotenv');
+dotenv.config();
+const token = process.env.TOKEN;
+
+bot.login(token);
