6e705ccc0ac1a7c57be5724c8300070efcb45584
fredrikjohanssonnn <fjohansson.creative@gmail.com>
2020-04-19 23:36:14 +0200

    Removed duplicated code

--- app.js
+++ app.js
@@ -91,15 +91,6 @@ bot.on('message', (message) => {
 
 const url = 'https://covid19.mathdro.id/api/countries/sweden';
 
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    const confirmed = data.confirmed.value;
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-
 bot.on('message', (message) => {
   if (!message.author.bot) {
     if (message.content.toLowerCase() == 'corona' || 'covid-19') {
